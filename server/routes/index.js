9f346b3d7c5a4a2e8415c93c4d864e6badcd9cd0
Ussada Arpamongkol <ussada.a@gmail.com>
2020-02-13 11:56:24 +0700

    Test web hook

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
     
     app.get('/api', (req, res) => {
         return res.status(200).send({
-            message: 'Welcome to my API'
+            message: 'Welcome'
         });
     });        
 
